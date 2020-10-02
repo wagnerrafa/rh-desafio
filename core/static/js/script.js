60d95b048e55c166c412426a009a544568f46f79
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-10-02 15:46:10 -0300

    css para atualizar models

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -2,6 +2,6 @@ function imagem(){
     document.getElementById("imagem").innerHTML="Imagem selecionada"
 }
 function formulario(){
-    document.getElementById("formulario").style.display = 'flex'
+    document.getElementById("formColaborador").style.display = 'block'
     
 }
