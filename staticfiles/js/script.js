6aa5caf8312fa4d1b0cdc92d31b656279199932a
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-10-02 13:44:24 -0300

    configuracoes para heroku

---
+++ staticfiles/js/script.js
@@ -0,0 +1,7 @@
+function imagem(){
+    document.getElementById("imagem").innerHTML="Imagem selecionada"
+}
+function formulario(){
+    document.getElementById("formulario").style.display = 'flex'
+    
+}
